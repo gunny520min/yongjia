3d2491fbf49cfe7ba8efb6f6ecbacad1bfa8486a
gunny520min <390922403@qq.com>
2015-07-27 11:09:52 +0800

    upload pic

--- src/main/java/com/yongjia/controller/web/CommonController.java
+++ src/main/java/com/yongjia/controller/web/CommonController.java
@@ -13,7 +13,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.yongjia.dao.UserMapper;
 import com.yongjia.model.User;
@@ -63,8 +65,8 @@ public class CommonController extends BaseController {
 
         Long userId = CookieUtil.getUserID(request);
         User user = userMapper.selectByPrimaryKey(userId);
-        if (user==null) {
-            return ToJsonUtil.toEntityMap(401, "error", null);
+        if (user == null) {
+            return ToJsonUtil.toEntityMap(401, "页面已失效，请重新登录", null);
         }
         return ToJsonUtil.toEntityMap(200, "success", user);
     }
@@ -79,8 +81,7 @@ public class CommonController extends BaseController {
 
     @RequestMapping("/modifyPwd")
     @ResponseBody
-    public Map modifyPwd(String oldpwd, String newpwd, HttpServletRequest request,
-            HttpServletResponse response) {
+    public Map modifyPwd(String oldpwd, String newpwd, HttpServletRequest request, HttpServletResponse response) {
 
         Long id = CookieUtil.getUserID(request);
         User user = userMapper.selectByPrimaryKey(id);
@@ -99,19 +100,38 @@ public class CommonController extends BaseController {
 
     @RequestMapping("/uploadPic")
     @ResponseBody
-    public Map uploadPic(File file, HttpServletRequest request, HttpServletResponse response) {
+    public Map uploadPic(@RequestParam(value = "file", required = true) MultipartFile file, HttpServletRequest request,
+            HttpServletResponse response) {
 
-        String imgName = DataUtils.getUUID();
+        String imgName = DataUtils.getUUID()+".jpg";
+        String path = request.getSession().getServletContext().getRealPath("upload");
+        String fileName = file.getOriginalFilename();
+        File targetFile = new File(path, fileName);
+        if (!targetFile.exists()) {
+            targetFile.mkdirs();
+        }
+        // 保存
         try {
-            String picUrl = UpYunUtil.uploadPic(file, imgName);
+            file.transferTo(targetFile);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        try {
+            String picUrl = UpYunUtil.uploadPic(targetFile, imgName);
             if (picUrl != null) {
                 Map<String, Object> map = new HashMap<String, Object>();
                 map.put("url", picUrl);
+                if (targetFile.exists()) {
+                    targetFile.delete();
+                }
                 return ToJsonUtil.toEntityMap(200, "success", picUrl);
             } else {
-                return ToJsonUtil.toEntityMap(400, "上传失败", null);
+                log.info("upload pic fail!");
+                String picUrl2 = request.getContextPath()+"/upload/"+fileName;
+                return ToJsonUtil.toEntityMap(200, "success", picUrl2);
             }
         } catch (IOException e) {
+            e.printStackTrace();
             return ToJsonUtil.toEntityMap(400, "上传失败", null);
         }
     }
